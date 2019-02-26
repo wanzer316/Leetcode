dca66264aac867131404080c418b85151c1fd95d
wanzer <wzgbm316@163.com>
2019-02-26 15:37:07 +0800

    "add file"

--- 1_two_sum/myself_01.py
+++ 1_two_sum/myself_01.py
@@ -4,3 +4,10 @@
 @Time    : 2019/2/26 15:30
 '''
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
